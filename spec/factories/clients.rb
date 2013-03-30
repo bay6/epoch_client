63ce729f78753b6d000e54dbef57a3ac8bc8a786
Ken Lu <ken@seeice.com>
2013-03-30 14:19:47 -0400

    test of scaffold

---
+++ spec/factories/clients.rb
@@ -0,0 +1,12 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :client do
+    name "MyString"
+    opening_hour "MyString"
+    address "MyString"
+    address "MyString"
+    phone_num "MyString"
+    description "MyText"
+  end
+end
