f6cf9e288cc3dd2fa836a493c60ca9929fff12ca
Ken Lu <ken@seeice.com>
2013-04-01 22:25:00 -0400

    added industry_list to client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :opening_hour, :phone_num, :avatar
+  attr_accessible :address, :description, :name, :opening_hour, :phone_num, :avatar, :industry_list
   mount_uploader :avatar, AvatarUploader
+  acts_as_taggable_on :industry
 end
