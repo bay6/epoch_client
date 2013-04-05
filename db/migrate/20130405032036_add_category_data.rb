572b8474e3b215c482850ab268af775f32f513bc
Ken Lu <ken@seeice.com>
2013-04-04 23:33:02 -0400

    adjust the data

--- db/migrate/20130405032036_add_category_data.rb
+++ db/migrate/20130405032036_add_category_data.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class AddCategoryData < ActiveRecord::Migration
   def up
-    %w('餐馆', '律师', '医生', '美容').each{|x| Category.create(name: x)}
+    %w(餐馆 律师 医生 美容).each{|x| Category.create(name: x)}
   end
 
   def down
