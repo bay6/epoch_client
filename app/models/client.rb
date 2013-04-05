2b339422d16582922d42d13643de467542ddcb70
Ken Lu <ken@seeice.com>
2013-04-04 23:32:44 -0400

    relation between client and category

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,6 @@
 class Client < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :opening_hour, :phone_num, :avatar, :industry_list
+  attr_accessible :address, :description, :name, :opening_hour, :phone_num, :avatar, :industry_list, :category, :category_id
   mount_uploader :avatar, AvatarUploader
   acts_as_taggable_on :industry
+  belongs_to :category
 end
