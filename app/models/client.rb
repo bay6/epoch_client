60bf9199fcf2f3d221ec4670b9586b8ca803055a
Ken Lu <ken@seeice.com>
2013-04-13 01:40:25 -0400

    ablity to update address

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ActiveRecord::Base
-  attr_accessible :address, :description, :name, :opening_hour, :phone_num, :avatar, :industry_list, :category, :category_id, :remove_avatar, :avatar_cache
+  attr_accessible :address, :description, :name, :opening_hour, :phone_num, :avatar, :industry_list, :category, :category_id, :remove_avatar, :avatar_cache, :latitude, :longitude
   mount_uploader :avatar, AvatarUploader
   acts_as_taggable_on :industry
   belongs_to :category
