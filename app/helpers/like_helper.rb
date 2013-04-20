96d062e95286ee076bee6cf133f945a510e1f62c
Ken Lu <ken@seeice.com>
2013-04-19 21:32:19 -0400

    like and visit display

--- app/helpers/like_helper.rb
+++ app/helpers/like_helper.rb
@@ -1,10 +1,10 @@
 #encoding: utf-8
 module LikeHelper
   def like_link_for(target)
-    link_to "喜欢", like_path(:resource_name => target  .class, :resource_id => target.id), :method => :post
+    link_to "赞", like_path(:resource_name => target  .class, :resource_id => target.id), :method => :post
   end
 
   def unlike_link_for(target)
-    link_to "取消喜欢", like_path(:resource_name => target.class, :resource_id => target.id), :method => :delete
+    link_to "取消赞", like_path(:resource_name => target.class, :resource_id => target.id), :method => :delete
   end
 end
