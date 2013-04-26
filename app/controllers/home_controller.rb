256ebea776c6e51295219a597d946d5a09109e00
songjiayang <songjiayang1@gmail.com>
2013-04-26 23:04:01 +0800

    添加了app 下载连接

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,8 @@ class HomeController < ApplicationController
   def index
     @users = User.all
   end
+
+  def download
+    send_file "#{Rails.root}/public/app/epoch-01.apk", :type=>"application/zip", :x_sendfile=>true
+  end
 end
