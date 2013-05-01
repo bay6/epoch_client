5c7e83853049abe9f4132db3940af58f1557242d
Ken Lu <ken@seeice.com>
2013-05-01 09:07:38 -0400

    annotate for spec

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: clients
+#
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  opening_hour :string(255)
+#  address      :string(255)
+#  phone_num    :string(255)
+#  description  :text
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  avatar       :string(255)
+#  category_id  :integer
+#  latitude     :float
+#  longitude    :float
+#
+
 require 'spec_helper'
 
 describe Client do
