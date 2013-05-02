3a63853ac1574c926236c31367c2ae4558075fa0
Ken Lu <ken@seeice.com>
2013-05-01 22:28:56 -0400

    added edit district

--- app/models/client.rb
+++ app/models/client.rb
@@ -18,7 +18,7 @@
 
 class Client < ActiveRecord::Base
   include Likeable
-  attr_accessible :address, :description, :name, :opening_hour, :phone_num, :avatar, :industry_list, :category, :category_id, :district, :remove_avatar, :avatar_cache, :latitude, :longitude
+  attr_accessible :address, :description, :name, :opening_hour, :phone_num, :avatar, :industry_list, :category, :category_id, :district, :district_id, :remove_avatar, :avatar_cache, :latitude, :longitude
   mount_uploader :avatar, AvatarUploader
   acts_as_taggable_on :industry
   belongs_to :category
